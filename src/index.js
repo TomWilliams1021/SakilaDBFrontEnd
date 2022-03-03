3d0198cb573e1e4b6e0d0e31d613d88262e0ef35
TomWilliams1021 <tomwilliams1021@gmail.com>
2022-03-03 14:04:27 +0000

    Updated Welcome text

--- src/index.js
+++ src/index.js
@@ -549,15 +549,13 @@ class MainPage extends React.Component {
     if (this.state.categoryDataFromServer.length !== 0) {
       return (
         <div id="siteWrapper">
-          <h1 id="siteName">My Favorite Movies</h1>
+          <h1 id="siteName">My Favorite Movies Blog</h1>
           <p id="aboutInfo">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            Welcome to My Favorite Movies Blog, on this site you can find
+            information on what I consider to be some of the best movies out
+            there. The movies are organised by category and can be selected to
+            view additional information about them such as starring actors,
+            release dates, run-time and more.
           </p>
 
           <CategorysTable
